3d31cb55cb0bf2aff87179c1259e72599d077b36
beppo <beppo@localhost.localdomain>
2021-01-28 12:22:19 -0600

    setting up search_live module

---
+++ lib/live_view_studio_web/live/search_live.ex
@@ -0,0 +1,65 @@
+defmodule LiveViewStudioWeb.SearchLive do
+  use LiveViewStudioWeb, :live_view
+
+  alias LiveViewStudio.Stores
+
+  def mount(_params, _session, socket) do
+    socket =
+      assign(socket,
+      zip: "",
+      stores: []
+    )
+
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~L"""
+    <h1>Find a Store</h1>
+    <div id="search">
+
+      <form phx-submit="zip-search">
+        <input type="text" name="zip" value="<%= @zip %>"
+              placeholder="Zip Code"
+              autofocus autocomplete="off" />
+
+        <button type="submit">
+          <img src="images/search.svg">
+        </button>
+      </form>
+
+      <div class="stores">
+        <ul>
+          <%= for store <- @stores do %>
+            <li>
+              <div class="first-line">
+                <div class="name">
+                  <%= store.name %>
+                </div>
+                <div class="status">
+                  <%= if store.open do %>
+                    <span class="open">Open</span>
+                  <% else %>
+                    <span class="closed">Closed</span>
+                  <% end %>
+                </div>
+              </div>
+              <div class="second-line">
+                <div class="street">
+                  <img src="images/location.svg">
+                  <%= store.street %>
+                </div>
+                <div class="phone_number">
+                  <img src="images/phone.svg">
+                  <%= store.phone_number %>
+                </div>
+              </div>
+            </li>
+          <% end %>
+        </ul>
+      </div>
+    </div>
+    """
+  end
+
+end
